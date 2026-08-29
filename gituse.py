b631f0d41d9ae01aff3b38295ace9baeb680c30b
minseongkim275-dotcom <minseongkim275@gmail.com>
2026-08-29 10:44:22 +0900

    git learning

---
+++ gituse.py
@@ -0,0 +1,12 @@
+"""
+git clone <템플릿-저장소-URL>
+cd [git clone으로 저장한 폴더 주소]
+rm -rf .git #숨겨진 깃 파일 삭제하기
+git init
+git remote add origin <본인-저장소-URL>
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main
+```
+"""
