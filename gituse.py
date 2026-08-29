04526890631cfa9b4b5514d7d4cee5fabe8c8fef
minseongkim275-dotcom <minseongkim275@gmail.com>
2026-08-29 10:52:05 +0900

    수정

--- gituse.py
+++ gituse.py
@@ -2,11 +2,27 @@
 git clone <템플릿-저장소-URL>
 cd [git clone으로 저장한 폴더 주소]
 rm -rf .git #숨겨진 깃 파일 삭제하기
-git init
-git remote add origin <본인-저장소-URL>
+git init    #새로운 저장소로 초기화
+git remote add origin <본인-저장소-URL> # remote는 주소록에 연락처를 등록하는 것 
+git set-url은 주소변경
 git add .
 git commit -m "Initial commit"
 git branch -M main
+
 git push -u origin main
+
+-u 없이: 매번 "누구한테 보낼지" 주소를 다 써야 하는 것
+-u 있으면: 한 번 "기본 수신자"로 등록해두고, 다음부턴 이름만 눌러도 보내지는 것
+
+
+git remote add origin
+git install -r requirements.txt
+
+
+python -m venv venv
+venv\Scripts\activate
+deactivate
+
+
 ```
 """
